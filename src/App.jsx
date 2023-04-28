1b6bfe25c2d75082293fee1403b6aebeed62a25d
Shuvam <shubhamduttadnk@gmail.com>
2023-04-28 10:49:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Profile from './components/Profile/Profile'
+
+function App() {
+  return (
+    <div className='container mt-5 shadow-sm'>
+      <Profile />
+    </div>
+  )
+}
+
+export default App
